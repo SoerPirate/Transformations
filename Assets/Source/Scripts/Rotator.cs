f2cff46993aa040bfeeae534ebec36ace7109634
SoerPirate <levdahnyuk@gmail.com>
2023-09-23 16:34:54 +0400

    Do "Rotator.cs"

--- Assets/Source/Scripts/Rotator.cs
+++ Assets/Source/Scripts/Rotator.cs
@@ -6,13 +6,20 @@ public class Rotator : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private Vector3 currentEulerAngles;
+    private Vector3 rotationDirection;
+
     void Start()
     {
-        // rotation Y
+        currentEulerAngles = transform.localEulerAngles;
+
+        rotationDirection = new Vector3(0, 1, 0);
     }
 
     void Update()
     {
-        
+        currentEulerAngles += rotationDirection * _speed * Time.deltaTime;
+
+        transform.localEulerAngles = currentEulerAngles;
     }
 }
